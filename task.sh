6324b8685c97fcf999a71da9ada3f7729d62a7e6
Mingfei Sun <minsun@savitar.cs.ox.ac.uk>
2020-10-21 22:49:38 +0100

    update

--- task.sh
+++ task.sh
@@ -1,6 +1,6 @@
-./run.sh 0 python3 runner.py --file=smac/arxiv_2s3z_torch_cv
-./run.sh 1 python3 runner.py --file=smac/arxiv_3s5z_vs_3s6z_torch_cv
-./run.sh 2 python3 runner.py --file=smac/arxiv_6h_vs_8z_torch_cv
-./run.sh 3 python3 runner.py --file=smac/arxiv_bane_vs_bane_torch_cv
-./run.sh 4 python3 runner.py --file=smac/arxiv_corridor_torch_cv
-./run.sh 5 python3 runner.py --file=smac/arxiv_MMM2_torch_cv
+./run.sh 0 python3 runner.py --file=whirl_baselines/2s3z_cv
+./run.sh 1 python3 runner.py --file=whirl_baselines/3s5z_vs_3s6z_cv
+./run.sh 2 python3 runner.py --file=whirl_baselines/6h_vs_8z_cv
+./run.sh 3 python3 runner.py --file=whirl_baselines/bane_vs_bane_cv
+./run.sh 4 python3 runner.py --file=whirl_baselines/corridor_cv
+./run.sh 5 python3 runner.py --file=whirl_baselines/MMM2_cv
